06602f49a2d3d64fb3194be9166970703dcd7832
Riya Sharma <74506498+RiyaSharma1199@users.noreply.github.com>
2020-12-04 13:12:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var gameObject1, gameObject2, gameObject3, gameObject4;
 
 function setup() {
   createCanvas(1200,800);
-  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect = createSprite(400, 400, 50, 80);
   fixedRect.shapeColor = "green";
  
   movingRect = createSprite(800, 400,80,30);
@@ -17,6 +17,9 @@ function setup() {
   gameObject3.shapeColor = "green";
   gameObject4 = createSprite(400, 100, 50, 50);
   gameObject4.shapeColor = "green";
+  
+  fixedRect.velocityY=-2;
+  gameObject4.velocityY=2;
 }
 
 function draw() {
@@ -32,6 +35,9 @@ function draw() {
     movingRect.shapeColor = "green";
     gameObject1.shapeColor = "green";
   }
+  
+   bounceOff(gameObject4,fixedRect);
+  
   drawSprites();
 }
 
@@ -45,5 +51,18 @@ function isTouching(object1,object2){
   }
   else {
     return false;
+  } 
+}
+
+function bounceOff(object1,object2){
+  if (object1.x - object2.x < object2.width/2 + object1.width/2
+    && object2.x - object1.x < object2.width/2 + object1.width/2) {
+    object1.velocityX = object1.velocityX * (-1);
+    object2.velocityX = object2.velocityX * (-1);
+  }
+  if (object1.y - object2.y < object2.height/2 + object1.height/2
+    && object2.y - object2.y < object2.height/2 + object1.height/2) {
+      object1.velocityY = object1.velocityY * (-1);
+      object2.velocityY = object2.velocityY * (-1);
   } 
 }
